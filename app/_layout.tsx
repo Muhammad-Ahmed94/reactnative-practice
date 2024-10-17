1b700edf1bfd8fa7d056f853037bee0d12237a30
Muhammad Ahmed <muneebbum757@gmail.com>
2024-10-17 09:33:40 +0500

    minor changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,22 +5,11 @@ import { Slot } from 'expo-router'
 import { Stack } from 'expo-router'
 
 const RootLayout = () => {
-  /* const handlePress = () => {
-    Alert.alert(
-      'Trivia questions',
-      'Are you a male?',
-
-      [
-        {text: 'Yes', onPress: () => console.log('you are a male')},
-        {text: 'No', onPress: () => console.log('you are Female')},
-      ]
-    )
-  } */
-
+  
   return (
     <>
     {/* <Slot /> */}
-    <Stack>
+    <Stack >
       <Stack.Screen name='index' options={{headerShown:false}} />
     </Stack>
     </>
@@ -29,12 +18,9 @@ const RootLayout = () => {
 
 export default RootLayout
 
-/* const styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     display: 'flex',
-    flex: 1,
-    gap: 4,
-    justifyContent: 'center',
-    alignItems: 'center',
+
   }
-}) */
+})
