74c3b0476458014d621630389b493675c0dc1ada
Muhammad Ahmed <muneebbum757@gmail.com>
2024-10-25 20:03:31 +0500

    custom button made

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,23 +1,43 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import colors from '@/constants/colors'
+import { coolDownAsync } from 'expo-web-browser'
 
 const CustomButton = () => {
   return (
-    <TouchableOpacity style={styles.customButton}>
-      <Text>CustomButton</Text>
-    </TouchableOpacity>
-  )
+    <View style= {styles.buttonViewStyle}>
+      <TouchableOpacity style={styles.customButtonStyle}>
+        <Text style={styles.buttonTextStyle}>CustomButton</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 export default CustomButton
 
 const styles = StyleSheet.create({
-    customButton: {
-        backgroundColor: 'orange',
-        paddingVertical: 2,
-        paddingHorizontal: 5,
-        color: 'black',
-        borderRadius: 10,
-        
-    }
-})
+  buttonViewStyle: {
+    height: "10%",
+    width: "50%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
+  },
+
+  customButtonStyle: {
+    backgroundColor: "orange",
+    paddingVertical: 2,
+    paddingHorizontal: 5,
+    color: "black",
+    borderRadius: 10,
+    justifyContent: "center",
+    height: "70%",
+    width: "100%",
+  },
+
+  buttonTextStyle: {
+    textAlign: "center",
+    color: `${colors.white}`,
+    fontSize: 25,
+  },
+});
