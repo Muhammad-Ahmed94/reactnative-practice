21bf173c824b4dc5af425a4cc82db7d769f9ff8d
Muhammad Ahmed <muneebbum757@gmail.com>
2024-10-29 22:08:10 +0500

    added comment

--- components/FormFeild.tsx
+++ components/FormFeild.tsx
@@ -2,6 +2,7 @@ import { StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
 import colors from '@/constants/colors';
 
+//* Add proper props attributes to read input values
 interface formFieldProps {
     title: string;
 }
