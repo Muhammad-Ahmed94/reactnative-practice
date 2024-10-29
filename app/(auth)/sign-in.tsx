501952919fe14294009eb5fffa164949da79e2e9
Muhammad Ahmed <muneebbum757@gmail.com>
2024-10-29 22:06:23 +0500

    feat: new screen add, added logo and main text

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,14 +1,52 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import colors from '@/constants/colors'
+import { images } from '@/constants'
+import FormFeild from '@/components/FormFeild'
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <>
+    <SafeAreaView style={styles.mainStyle}>
+      <ScrollView contentContainerStyle={{height: '100%'}}>
+        <View style={styles.viewStyle}>
+          <Image source={images.logo} resizeMode='contain' style={styles.logo} />
+          <Text style={styles.text}>Log In To Aora</Text>
+        </View>
+
+        <View>
+          <FormFeild title='email' />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+    </>
   )
 }
 
 export default SignIn
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  mainStyle: {
+    backgroundColor: colors.primary,
+    height: "100%",
+  },
+
+  viewStyle: {
+    width: "100%",
+    flex: 1,
+    padding: 20,
+  },
+
+  logo: {
+    width: "30%",
+    height: "10%",
+  },
+
+  text: {
+    color: colors.white,
+    marginTop: 10,
+    fontSize: 25,
+    fontWeight: 'bold',
+  }
+});
---
+++ components/FormFeild.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+import React from 'react'
+import colors from '@/constants/colors';
+
+interface formFieldProps {
+    title: string;
+}
+
+const FormFeild:React.FC<formFieldProps> = ({ title }) => {
+  return (
+    <View style={styles.viewStyle}>
+        <Text style={{color: colors.white}}>{title}</Text>
+    </View>
+  )
+}
+
+export default FormFeild
+
+const styles = StyleSheet.create({
+  viewStyle: {
+    height: 20,
+    width: '100%',
+    borderWidth: 1,
+    borderColor: 'orange'
+  }
+});
