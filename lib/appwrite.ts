c95b47009a94dc4e9647377839bfaa0c0f3c9bb7
Muhammad Ahmed <muneebbum757@gmail.com>
2024-10-31 23:42:35 +0500

    appwrite package installed

---
+++ lib/appwrite.ts
@@ -0,0 +1,8 @@
+export const appwriteConfig = {
+  endpoint: "https://cloud.appwrite.io/v1",
+  platform: "com.muneeb.aora_",
+  projectId: "6723cdac003bce849128",
+  databseId: "6723cf2b0018a6fd80cc",
+  usersCollectionId: "6723cf44000227c56e24",
+  videosCollectionId: "6723cf75003a48832b67",
+};
