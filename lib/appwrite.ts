dd8292ae7e53f44272bc03a9524ea595314a9abf
Muhammad Ahmed <muneebbum757@gmail.com>
2024-10-31 23:59:06 +0500

    appwirte config commplete

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,3 +1,5 @@
+import { Client } from "react-native-appwrite";
+
 export const appwriteConfig = {
   endpoint: "https://cloud.appwrite.io/v1",
   platform: "com.muneeb.aora_",
@@ -5,4 +7,13 @@ export const appwriteConfig = {
   databseId: "6723cf2b0018a6fd80cc",
   usersCollectionId: "6723cf44000227c56e24",
   videosCollectionId: "6723cf75003a48832b67",
+  storageId: "6723d1c10029a8b4bf3e",
 };
+
+// Init your React Native SDK
+const client = new Client();
+
+client
+  .setEndpoint(appwriteConfig.endpoint) // Your Appwrite Endpoint
+  .setProject(appwriteConfig.projectId) // Your project ID
+  .setPlatform(appwriteConfig.platform); // Your application ID or bundle ID.
