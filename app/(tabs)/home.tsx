3fa3b0e011d97668db8688a8a8756280417fb330
Muhammad Ahmed <muneebbum757@gmail.com>
2024-12-03 22:09:52 +0500

    Home Screen: Home title and search input field added

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,14 +1,77 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, Image, StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import colors from '@/constants/colors'
+import { images } from '@/constants'
+import SearchInput from '@/components/SearchInput'
 
 const Home = () => {
   return (
-    <View>
-      <Text>This is home page</Text>
-    </View>
-  )
+    <SafeAreaView style={{ backgroundColor: colors.primary }}>
+      <FlatList
+        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <Text style={{ fontSize: 40, color: colors.white }}>{item.id}</Text>
+        )}
+        ListHeaderComponent={() => (
+          <View style={styles.homeContainer}>
+            <View style={styles.homeInnerContainer}>
+              <View style={styles.homeTitleContainer}>
+                <Text style={[styles.textWhite, styles.homeTitle]}>Welcome Muneeb!</Text>
+                <Image source={images.logoSmall} resizeMode='contain' style={{height: 30, width: 30}} />
+              </View>
+              
+              <SearchInput placeholder='search video you want' />
+            </View>
+          </View>
+        )}
+      />
+    </SafeAreaView>
+  );
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  centerAlign: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  textWhite: {
+    color: colors.white,
+  },
+
+  homeContainer: {
+    marginVertical: 25,
+    marginHorizontal: 20,
+  },
+
+  homeInnerContainer: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignContent: "flex-start",
+  },
+
+  homeTitleContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+
+  homeTitle: {
+    fontSize: 20,
+  },
+
+  /* homeSearchInput: {
+    height: 28,
+    width: '100%',
+    padding: 15, 
+    borderRadius: 8,
+    borderColor: colors.white,
+    marginHorizontal: 20,
+    color: 'gray',
+    fontSize: 20
+    
+  } */
+});
---
+++ components/SearchInput.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Image, StyleSheet, TextInput, TouchableOpacity, View } from 'react-native';
+
+import { icons } from '@/constants';
+import colors from '@/constants/colors';
+
+//* Add proper props attributes to read input values
+interface searchInputProps {
+  value?: string;
+  handleTextChange?: (text: string) => void;
+  placeholder: string;
+}
+
+const SearchInput: React.FC<searchInputProps> = ({
+  value,
+  handleTextChange,
+  placeholder,
+}) => {
+  return (
+    <View style={styles.searchInputCotainer}>
+      <View style={[styles.searchInput]}>
+        <TextInput
+          value={value}
+          onChangeText={handleTextChange}
+          placeholder={placeholder}
+          placeholderTextColor="gray"
+          style={styles.textInput}
+        />
+        <TouchableOpacity>
+            <Image source={icons.search} resizeMode="contain" style={{height: 20, width: 20}} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default SearchInput;
+
+const styles = StyleSheet.create({
+  searchInputCotainer: {
+    width: "100%",
+    marginVertical: 20,
+  },
+
+  textStyle: {
+    color: colors.white,
+    marginBottom: 10,
+    fontSize: 20,
+  },
+
+  searchInput: {
+    width: "100%",
+    height: 55,
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 10,
+    marginBottom: 10,
+    padding: 15,
+    backgroundColor: "black",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+
+  textInput: {
+    color: colors.white,
+    fontSize: 20,
+  },
+});
