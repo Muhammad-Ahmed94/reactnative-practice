c08abbfa7311640a4757acfd65646f51580af6be
Muhammad-Ahmad94 <muneebbum757@gmail.com>
2024-12-24 20:22:13 +0500

    Style: Create Screen style complete. Upload video function is remain

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -5,6 +5,7 @@ import FormFeild from '@/components/FormFeild';
 import { ResizeMode, Video } from 'expo-av';
 import { icons } from '@/constants';
 import { FlipInEasyX } from 'react-native-reanimated';
+import CustomButton from '@/components/CustomButton';
 
 const Create = () => {
   const [ form, setForm ] = useState({
@@ -13,59 +14,94 @@ const Create = () => {
     thumbnail: null,
     prompt: ''
   })
+  const [ uploading, setIsUploading ] =useState(false)
+
+  const documentPicker = () => {
+
+  }
+
+  const submitVideo = () => {
+
+  }
 
   return (
-    <SafeAreaView style={{backgroundColor: colors.primary, height: '100%'}}>
-      <ScrollView style={{paddingHorizontal: 20, marginVertical: 18}}>
-        <Text style={{color:'white', fontSize: 25, marginBottom: 30}}>Upload Videos</Text>
-        <FormFeild 
-        title='Video Title'
-        value={form.title}
-        placeholder='Give your video a title...'
-        handleTextChange={(e) => setForm({...form, title: e})}
+    <SafeAreaView style={{ backgroundColor: colors.primary, height: "100%" }}>
+      <ScrollView style={{ paddingHorizontal: 20, marginVertical: 18 }}>
+        <Text style={{ color: "white", fontSize: 25, marginBottom: 30 }}>
+          Upload Videos
+        </Text>
+        <FormFeild
+          title="Video Title"
+          value={form.title}
+          placeholder="Give your video a title..."
+          handleTextChange={(e) => setForm({ ...form, title: e })}
         />
 
-        <View style={styles.uploadVideoContainer}>
-          <Text style={styles.colorWhite}>Upload your video here</Text>
-
-          <TouchableOpacity>
-            {form.video ? (
-              <Video 
-              source={{uri: form.video.uri}} 
-              resizeMode={ResizeMode.COVER}
-              useNativeControls
-              shouldPlay
-              isLooping
-              />
-            ) : (
-              <View style={[styles.uploadVideoContent]}>
-                <View style={[styles.uploadVideoIcon, styles.alignCenter]}>
-                  <Image source={icons.upload} resizeMode='contain' style={{padding: 15}} height={15} width={1} />
-                </View>
-
-              </View>
-            )}
-          </TouchableOpacity>
-        </View>
+        <FormFeild
+          title="Video Prompt"
+          value={form.prompt}
+          placeholder="Prompt used to create the video"
+          handleTextChange={(e) => setForm({ ...form, prompt: e })}
+        />
 
         <View style={styles.uploadVideoContainer}>
           <Text style={styles.colorWhite}>Thumbnail Image</Text>
 
           <TouchableOpacity>
             {form.thumbnail ? (
-              <Image 
-              source={{uri: form.thumbnail.uri}} 
-              resizeMode='contain'
+              <Image
+                source={{ uri: form.thumbnail.uri }}
+                resizeMode="contain"
+              />
+            ) : (
+              <View style={[styles.uploadVideoContent, { height: 70, }]}>
+                <View style={[styles.uploadVideoIcon, styles.alignCenter, {flexDirection: 'row', justifyContent: 'center', alignItems: 'center', width: '100%', gap: 5}]}>
+                  <Image
+                    source={icons.upload}
+                    resizeMode="contain"
+                    style={{ padding: 15, height: 10, width: 10 }}
+                  />
+                  <Text style={{color: colors.secondary, fontWeight: 'bold', fontSize: 15}}>Video Thumbnail</Text>
+                </View>
+              </View>
+            )}
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.uploadVideoContainer}>
+          <Text style={styles.colorWhite}>Upload your video here</Text>
+
+          <TouchableOpacity>
+            {form.video ? (
+              <Video
+                source={{ uri: form.video.uri }}
+                resizeMode={ResizeMode.COVER}
+                useNativeControls
+                shouldPlay
+                isLooping
               />
             ) : (
               <View style={[styles.uploadVideoContent]}>
                 <View style={[styles.uploadVideoIcon, styles.alignCenter]}>
-                  <Image source={icons.upload} resizeMode='contain' style={{padding: 15}} height={15} width={1} />
+                  <Image
+                    source={icons.upload}
+                    resizeMode="contain"
+                    style={{ padding: 15 }}
+                    height={15}
+                    width={1}
+                  />
                 </View>
-
               </View>
             )}
           </TouchableOpacity>
+        </View>
+
+        <View style={{ marginTop: 10, alignItems: "center" }}>
+          <CustomButton
+            title="Submit & Publish"
+            isLoading={uploading}
+            handlePress={submitVideo}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -90,8 +126,7 @@ const styles = StyleSheet.create({
   },
 
   uploadVideoContent: {
-    borderWidth: 1,
-    borderColor: "red",
+    backgroundColor: "#4A4A51",
     borderRadius: 15,
     marginTop: 10,
     width: "100%",
@@ -101,9 +136,6 @@ const styles = StyleSheet.create({
 
   uploadVideoIcon: {
     padding: 8,
-    borderWidth: 1,
-    borderStyle: "dotted",
-    borderColor: "yellow",
     borderRadius: 15,
     display: "flex",
     height: 60,
@@ -111,9 +143,8 @@ const styles = StyleSheet.create({
     position: "relative",
     top: "50%",
     left: "50%",
-    transform: [{translateY: '-50%'}, { translateX: '-50%'}],
+    transform: [{ translateY: "-50%" }, { translateX: "-50%" }],
     /*justifyContent: 'center',
     alignItems: 'center'*/
   },
-
 });
