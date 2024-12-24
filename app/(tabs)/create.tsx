dae3e96e518d7cc1c79fe5bcc60f5b209570d514
Muhammad-Ahmad94 <muneebbum757@gmail.com>
2024-12-24 19:17:01 +0500

    Add: Upload video and thumbnail section in CREATE screen.

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,7 +1,10 @@
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { Image, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
 import colors from '@/constants/colors';
 import FormFeild from '@/components/FormFeild';
+import { ResizeMode, Video } from 'expo-av';
+import { icons } from '@/constants';
+import { FlipInEasyX } from 'react-native-reanimated';
 
 const Create = () => {
   const [ form, setForm ] = useState({
@@ -21,6 +24,49 @@ const Create = () => {
         placeholder='Give your video a title...'
         handleTextChange={(e) => setForm({...form, title: e})}
         />
+
+        <View style={styles.uploadVideoContainer}>
+          <Text style={styles.colorWhite}>Upload your video here</Text>
+
+          <TouchableOpacity>
+            {form.video ? (
+              <Video 
+              source={{uri: form.video.uri}} 
+              resizeMode={ResizeMode.COVER}
+              useNativeControls
+              shouldPlay
+              isLooping
+              />
+            ) : (
+              <View style={[styles.uploadVideoContent]}>
+                <View style={[styles.uploadVideoIcon, styles.alignCenter]}>
+                  <Image source={icons.upload} resizeMode='contain' style={{padding: 15}} height={15} width={1} />
+                </View>
+
+              </View>
+            )}
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.uploadVideoContainer}>
+          <Text style={styles.colorWhite}>Thumbnail Image</Text>
+
+          <TouchableOpacity>
+            {form.thumbnail ? (
+              <Image 
+              source={{uri: form.thumbnail.uri}} 
+              resizeMode='contain'
+              />
+            ) : (
+              <View style={[styles.uploadVideoContent]}>
+                <View style={[styles.uploadVideoIcon, styles.alignCenter]}>
+                  <Image source={icons.upload} resizeMode='contain' style={{padding: 15}} height={15} width={1} />
+                </View>
+
+              </View>
+            )}
+          </TouchableOpacity>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -28,4 +74,46 @@ const Create = () => {
 
 export default Create
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  alignCenter: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  colorWhite: {
+    color: colors.white,
+    fontSize: 20,
+  },
+
+  uploadVideoContainer: {
+    marginTop: 10,
+  },
+
+  uploadVideoContent: {
+    borderWidth: 1,
+    borderColor: "red",
+    borderRadius: 15,
+    marginTop: 10,
+    width: "100%",
+    height: 200,
+    display: "flex",
+  },
+
+  uploadVideoIcon: {
+    padding: 8,
+    borderWidth: 1,
+    borderStyle: "dotted",
+    borderColor: "yellow",
+    borderRadius: 15,
+    display: "flex",
+    height: 60,
+    width: 60,
+    position: "relative",
+    top: "50%",
+    left: "50%",
+    transform: [{translateY: '-50%'}, { translateX: '-50%'}],
+    /*justifyContent: 'center',
+    alignItems: 'center'*/
+  },
+
+});
